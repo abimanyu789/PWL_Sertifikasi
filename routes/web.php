1a8b4a4511390e54514e84ef06bcf1bc4a801cf7
AldamaitaSS <pijarheksa02@gmail.com>
2024-11-17 07:54:10 +0700

    Profile

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,11 @@ use App\Http\Controllers\BidangController;
 use App\Http\Controllers\JenisSertifikasiController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\LevelPelatihanController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PelatihanController;
+use App\Http\Controllers\SertifikasiController;
 use App\Http\Controllers\WelcomeController;
 
 use Illuminate\Support\Facades\Route;
@@ -30,6 +33,11 @@ Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
 Route::middleware(['auth'])->group(function () {
     Route::get('/', [WelcomeController::class, 'index']); //halaman awal
     
+    Route::group(['middleware' => 'authorize:ADM'], function () {
+        Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
+        Route::patch('/profile/update/{id}', [ProfileController::class, 'update'])->name('profile.update');
+    }); 
+
     // Route::group(['prefix' => 'user'], function () {
     Route::group(['prefix' => 'user', 'middleware'=> 'authorize:ADM'], function(){
         Route::get('/', [UserController::class, 'index']);          
@@ -68,6 +76,34 @@ Route::middleware(['auth'])->group(function () {
     Route::group(['prefix' => 'level_pelatihan', 'middleware'=> 'authorize:ADM'], function(){
         Route::get('/', [LevelPelatihanController::class, 'index']);          
         Route::post('/list', [LevelPelatihanController::class, 'list']);      
+    });
+
+    Route::group(['prefix' => 'pelatihan', 'middleware'=> 'authorize:ADM'], function(){
+        Route::get('/', [PelatihanController::class, 'index']);
+        Route::post('/list', [PelatihanController::class, 'list']);
+        Route::get('/create_ajax', [PelatihanController::class, 'create_ajax']);
+        Route::post('/ajax', [PelatihanController::class, 'store_ajax']);
+        Route::get('/{id}/show_ajax', [PelatihanController::class, 'show_ajax']);
+        Route::get('/{id}/edit_ajax', [PelatihanController::class, 'edit_ajax']);
+        Route::put('/{id}/update_ajax', [PelatihanController::class, 'update_ajax']);
+        Route::get('/{id}/delete_ajax', [PelatihanController::class, 'confirm_ajax']);
+        Route::delete('/{id}/delete_ajax', [PelatihanController::class, 'delete_ajax']);
+    });
+
+    Route::group(['prefix' => 'level_pelatihan', 'middleware'=> 'authorize:ADM'], function(){
+
+    });
+
+    Route::group(['prefix' => 'sertifikasi', 'middleware'=> 'authorize:ADM'], function(){
+        Route::get('/', [SertifikasiController::class, 'index']);
+        Route::post('/list', [SertifikasiController::class, 'list']);
+        Route::get('/create_ajax', [SertifikasiController::class, 'create_ajax']);
+        Route::post('/ajax', [SertifikasiController::class, 'store_ajax']);
+        Route::get('/{id}/show_ajax', [SertifikasiController::class, 'show_ajax']);
+        Route::get('/{id}/edit_ajax', [SertifikasiController::class, 'edit_ajax']);
+        Route::put('/{id}/update_ajax', [SertifikasiController::class, 'update_ajax']);
+        Route::get('/{id}/delete_ajax',[SertifikasiController::class, 'confirm_ajax']);
+        Route::delete('/{id}/delete_ajax', [SertifikasiController::class, 'delete_ajax']);
     });
 
     Route::post('logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
