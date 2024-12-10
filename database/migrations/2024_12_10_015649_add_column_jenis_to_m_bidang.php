61ae9ff6327894032d118ee1c981bf825f35dbdd
abimanyu789 <anandaabimanyu7@gmail.com>
2024-12-11 00:57:09 +0700

    add column jenis to m_bidang

---
+++ database/migrations/2024_12_10_015649_add_column_jenis_to_m_bidang.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('m_bidang', function (Blueprint $table) {
+            $table->unsignedBigInteger('jenis_id')->after('bidang_nama');
+
+            $table->foreign('jenis_id')->references('jenis_id')->on('m_jenis');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('m_bidang', function (Blueprint $table) {
+            $table->dropColumn('jenis_id');
+        });
+    }
+};
