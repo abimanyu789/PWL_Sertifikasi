a089be9462b4dce8aa9341e7632bc805914c91f6
AldamaitaSS <pijarheksa02@gmail.com>
2024-12-10 07:13:49 +0700

    Level seeder

--- database/seeders/LevelSertifikasiSeeder.php
+++ database/seeders/LevelSertifikasiSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class JenisSertifikasiSeeder extends Seeder
+class LevelSertifikasiSeeder extends Seeder
 {
     /**
      * Run the database seeds.
