299e835a99e32a910a9093f5733a7ecc60a5b216
Abel <evilfighterthirdacc@gmail.com>
2024-12-11 08:38:18 +0700

    Fix Import button pelatihan, sertifikasi

--- resources/views/data_pelatihan/pelatihan/index.blade.php
+++ resources/views/data_pelatihan/pelatihan/index.blade.php
@@ -5,7 +5,7 @@
         <div class="card-header">
             <h3 class="card-title">{{ $page->title }}</h3>
             <div class="card-tools">
-                <button onclick="modalAction('{{ url('/pelatihan/import_ajax') }}')" class="btn btn-sm btn-info mt-1">Import</button>
+                <button onclick="modalAction('{{ url('/pelatihan/import') }}')" class="btn btn-sm btn-info mt-1">Import</button>
                 <a class="btn btn-sm btn-primary mt-1" h href="{{ url('/pelatihan/export_excel') }}" class="btn btn-primary"><i class="fa fa-file-excel"></i> Export (Excel)</a>
                 <a class="btn btn-sm btn-warning mt-1" href="{{ url('/pelatihan/export_pdf') }}" class="btn btn-warning"><i class="fa fa-file-pdf"></i> Export (PDF)</a>
                 <button onclick="modalAction('{{ url('/pelatihan/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah</button>
--- resources/views/data_sertifikasi/sertifikasi/index.blade.php
+++ resources/views/data_sertifikasi/sertifikasi/index.blade.php
@@ -5,7 +5,7 @@
         <div class="card-header">
             <h3 class="card-title">{{ $page->title }}</h3>
             <div class="card-tools">
-                <button onclick="modalAction('{{ url('/sertifikasi/import_ajax') }}')" class="btn btn-sm btn-info mt-1">Import</button>
+                <button onclick="modalAction('{{ url('/sertifikasi/import') }}')" class="btn btn-sm btn-info mt-1">Import</button>
                 <a class="btn btn-sm btn-primary mt-1" h href="{{ url('/sertifikasi/export_excel') }}" class="btn btn-primary"><i class="fa fa-file-excel"></i> Export (Excel)</a>
                 <a class="btn btn-sm btn-warning mt-1" href="{{ url('/sertifikasi/export_pdf') }}" class="btn btn-warning"><i class="fa fa-file-pdf"></i> Export (PDF)</a>
                 <button onclick="modalAction('{{ url('/sertifikasi/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah</button>
